f745bc3c7f7909940303ed4124bc0d4e4342a68f
Noa Goldshmid <noa@frontegg.com>
2022-01-04 11:25:25 +0200

    FR-5280 CR changes

--- src/authz/index.ts
+++ src/authz/index.ts
@@ -1,8 +1,6 @@
-import axios from 'axios';
 import {FronteggAuthenticator} from "../authenticator";
-import { config } from '../config';
-import Logger from "../helpers/logger";
 import {StrategyType} from "./strategy-types";
+import {FronteggStrategy, IAuthzStrategy, OpaStrategy} from "./strategies";
 
 export class AuthzClient {
 
@@ -43,48 +41,3 @@ export interface IOpaAuthzOptions {
     strategy?: StrategyType.Opa;
 }
 
-interface IAuthzStrategy {
-    hasScopes(auth: string | FronteggAuthenticator, user:object, scope: string[], assetId?: string): Promise<boolean>;
-}
-
-class OpaStrategy implements IAuthzStrategy {
-
-    public async hasScopes(clientId: string, user: object, scope: string[], assetId?: string): Promise<boolean> {
-        try {
-            Logger.info('going to verify scope');
-
-            clientId = clientId.replace(/-/g, '');
-
-            const input: {[k: string]: any} = {};
-            input["scope"] = scope[0]; // TODO: once decided if we use array or not for scopes, change it accordingly
-            if(assetId) {
-                input["assetId"] = assetId;
-            }
-            Object.keys(user).forEach(
-                e => {
-                    input[`${e}`] = user[`${e}`];
-                }
-            );
-            const opaPrefix = 'frontegg/opa';
-            const { data } = await axios.post(`${config.urls.authzService}/v1/data/${opaPrefix}/${clientId}/allow`,
-                { input }, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        },
-                responseType: 'json',
-                });
-            Logger.info('verified scope successfully');
-            return data.result
-        } catch (e) {
-            Logger.error('failed to verify scope in authz service - ', e);
-            throw e;
-        }
-    }
-}
-
-class FronteggStrategy implements IAuthzStrategy {
-
-    public async hasScopes(clientId: FronteggAuthenticator, user: object, scope: Array<string>, assetId?: string): Promise<boolean> {
-        return true;
-    }
-}
