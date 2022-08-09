586c485a942281e56b8b16c811c45054143a98f9
sapir-frontegg <sapir@frontegg.com>
2022-08-09 15:36:25 +0300

    FR-8294 - Handle error rejection

--- src/middleware/fronteggMiddleware.ts
+++ src/middleware/fronteggMiddleware.ts
@@ -5,10 +5,20 @@ import { getPackageJson } from '../utils/getPackageJSON';
 import { ContextHolder } from './ContextHolder';
 import { fronteggRoutes } from './FronteggRoutes';
 import { IFronteggOptions } from './types';
-import { callMiddleware, enableCors, rewriteCookieDomain, validatePermissions } from './utils';
+import {
+  callMiddleware,
+  enableCors,
+  rewriteCookieDomain,
+  validatePermissions,
+} from './utils';
 
-const proxy = httpProxy.createProxyServer({ secure: false, changeOrigin: true, xfwd: true });
-const target = process.env.FRONTEGG_API_GATEWAY_URL || 'https://api.frontegg.com/';
+const proxy = httpProxy.createProxyServer({
+  secure: false,
+  changeOrigin: true,
+  xfwd: true,
+});
+const target =
+  process.env.FRONTEGG_API_GATEWAY_URL || 'https://api.frontegg.com/';
 
 const pjson = getPackageJson() || { version: 'unknown' };
 
@@ -16,20 +26,28 @@ const MAX_RETRIES = 3;
 
 async function proxyRequest(req, res, context, authenticator) {
   await authenticator.validateAuthentication();
-  Logger.log(`going to proxy request - ${req.originalUrl} to ${target}`);
+  Logger.log(
+    `going to proxy request - ${req.originalUrl} to ${target}`
+  );
 
   const headers = {
     'x-access-token': authenticator.accessToken,
-    'frontegg-tenant-id': context && context.tenantId ? context.tenantId : 'WITHOUT_TENANT_ID',
-    'frontegg-user-id': context && context.userId ? context.userId : '',
+    'frontegg-tenant-id':
+      context && context.tenantId
+        ? context.tenantId
+        : 'WITHOUT_TENANT_ID',
+    'frontegg-user-id':
+      context && context.userId ? context.userId : '',
     'frontegg-vendor-host': req.hostname,
     'frontegg-middleware-client': `Node.js@${pjson.version}`,
     'frontegg-authenticated-entity-id': context.authenticatedEntityId,
-    'frontegg-authenticated-entity-type': context.authenticatedEntityType
+    'frontegg-authenticated-entity-type':
+      context.authenticatedEntityType,
   };
 
   if (context.userPermissions) {
-    headers['frontegg-user-permissions'] = context.userPermissions.join(',');
+    headers['frontegg-user-permissions'] =
+      context.userPermissions.join(',');
   }
 
   await proxy.web(req, res, {
@@ -37,7 +55,6 @@ async function proxyRequest(req, res, context, authenticator) {
     headers,
   });
 }
-
 
 export function frontegg(options: IFronteggOptions) {
   const { maxRetries = MAX_RETRIES } = options;
@@ -56,7 +73,6 @@ export function frontegg(options: IFronteggOptions) {
     throw new Error('Missing context resolver');
   }
 
-
   const authenticator = new FronteggAuthenticator();
 
   ContextHolder.setContext({
@@ -64,12 +80,26 @@ export function frontegg(options: IFronteggOptions) {
     FRONTEGG_API_KEY: options.apiKey,
   });
 
-  let authInitedPromise = authenticator.init(options.clientId, options.apiKey);
+  let authInitedPromise = authenticator.init(
+    options.clientId,
+    options.apiKey
+  );
 
   proxy.on('error', async (err, req: any, res, _) => {
     Logger.error(`Failed proxy request to ${req.url} - `, err);
     req.frontegg.retryCount++;
-    Logger.info(`retry count of ${req.url} - `, req.frontegg.retryCount);
+    Logger.info(
+      `retry count of ${req.url} - `,
+      req.frontegg.retryCount
+    );
+
+    if (res.headersSent) {
+      // response was already sent from the middleware, we have nothing left to do
+      Logger.debug(
+        'error has been thrown, but headers was already sent'
+      );
+      return;
+    }
 
     if (req.frontegg.retryCount >= maxRetries) {
       res.writeHead(500).end('Frontegg request failed');
@@ -99,12 +129,15 @@ export function frontegg(options: IFronteggOptions) {
 
       Object.keys(proxyRes.headers).forEach((key) => {
         if (key.toLowerCase() === 'set-cookie') {
-          proxyRes.headers[key] = rewriteCookieDomain(proxyRes.headers[key], host, options.cookieDomainRewrite);
+          proxyRes.headers[key] = rewriteCookieDomain(
+            proxyRes.headers[key],
+            host,
+            options.cookieDomainRewrite
+          );
         }
       });
     }
   });
-
 
   proxy.on('proxyReq', (proxyReq, req: any, res, _) => {
     try {
@@ -117,20 +150,29 @@ export function frontegg(options: IFronteggOptions) {
       proxyReq.removeHeader('Authorization');
 
       if (req.body) {
-        let contentType = proxyReq.getHeader('Content-Type') as string;
-        let contentLength = proxyReq.getHeader('Content-Length') as number;
-        if (contentType && contentType.startsWith('multipart/form-data')) {
+        let contentType = proxyReq.getHeader(
+          'Content-Type'
+        ) as string;
+        let contentLength = proxyReq.getHeader(
+          'Content-Length'
+        ) as number;
+        if (
+          contentType &&
+          contentType.startsWith('multipart/form-data')
+        ) {
           proxyReq.setHeader('Content-Type', contentType);
           proxyReq.setHeader('Content-Length', contentLength);
           return;
         }
-        const bodyData = Buffer.isBuffer(req.body) ? req.body : JSON.stringify(req.body);
+        const bodyData = Buffer.isBuffer(req.body)
+          ? req.body
+          : JSON.stringify(req.body);
         contentType = 'application/json';
         contentLength = Buffer.byteLength(bodyData);
         proxyReq.write(bodyData);
       }
     } catch (e) {
-      Logger.error('could not proxy request to frontegg', ...e)
+      Logger.error('could not proxy request to frontegg', ...e);
     }
   });
 
@@ -140,24 +182,34 @@ export function frontegg(options: IFronteggOptions) {
       await authInitedPromise;
     } catch (e) {
       Logger.error('Failed to authenticate via promise - ', e);
-      authInitedPromise = authenticator.init(options.clientId, options.apiKey);
+      authInitedPromise = authenticator.init(
+        options.clientId,
+        options.apiKey
+      );
       throw e;
     }
-    
-    if (options.authMiddleware && !await fronteggRoutes.isFronteggPublicRoute(req)) {
+
+    if (
+      options.authMiddleware &&
+      !(await fronteggRoutes.isFronteggPublicRoute(req))
+    ) {
       Logger.debug('will pass request threw the auth middleware');
       try {
         await callMiddleware(req, res, options.authMiddleware);
         if (res.headersSent) {
           // response was already sent from the middleware, we have nothing left to do
-          Logger.debug('Headers was already sent from authMiddleware');
+          Logger.debug(
+            'Headers was already sent from authMiddleware'
+          );
           return;
         }
       } catch (e) {
         Logger.error(`Failed to call middleware - `, e);
         if (res.headersSent) {
           // response was already sent from the middleware, we have nothing left to do
-          Logger.debug('authMiddleware threw error, but headers was already sent');
+          Logger.debug(
+            'authMiddleware threw error, but headers was already sent'
+          );
           return;
         }
         return res.status(401).send(e.message);
@@ -170,7 +222,9 @@ export function frontegg(options: IFronteggOptions) {
 
     if (req.method === 'OPTIONS') {
       // tslint:disable-next-line:no-console
-      console.log('OPTIONS call to frontegg middleware - returning STATUS 204');
+      console.log(
+        'OPTIONS call to frontegg middleware - returning STATUS 204'
+      );
       res.status(204).send();
       return;
     }
@@ -192,6 +246,3 @@ export function frontegg(options: IFronteggOptions) {
     return proxyRequest(req, res, context, authenticator);
   };
 }
-
-
-
