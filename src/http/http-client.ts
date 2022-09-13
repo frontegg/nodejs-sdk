fa2930e7cdb2f70bfbd4592db6865d9096bbc93e
Julian <jklumpers@koppert.nl>
2022-09-13 02:21:59 +0200

    refactor(http-client): simplified api and added interceptor
    Added interceptor for setting x-access-token header
    Added axios config for setting base url for example
    Added possibility to add Typescript generics for the api reponses

--- src/http/http-client.ts
+++ src/http/http-client.ts
@@ -1,56 +1,58 @@
-import axios from "axios";
+import Axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 import { FronteggAuthenticator } from "../authenticator";
 
 export class HttpClient {
-  constructor(private readonly authenticator: FronteggAuthenticator) {}
+  axios: AxiosInstance;
 
-  public async get(url: string, heeders: any): Promise<any> {
-    await this.authenticator.validateAuthentication();
-    return axios.get(url, {
-      headers: {
-        ...(heeders || {}),
-        "x-access-token": this.authenticator.accessToken,
-      },
-    });
+  constructor(
+    private readonly authenticator: FronteggAuthenticator,
+    axiosConfig?: AxiosRequestConfig
+  ) {
+    this.axios = Axios.create(axiosConfig);
+    this.setupRequestInterceptor();
   }
 
-  public async post(url: string, data: any, headers: any): Promise<any> {
-    await this.authenticator.validateAuthentication();
-    return axios.post(url, data, {
-      headers: {
-        ...(headers || {}),
-        "x-access-token": this.authenticator.accessToken,
+  private setupRequestInterceptor() {
+    this.axios.interceptors.request.use(
+      (request: AxiosRequestConfig) => {
+        request.headers = {
+          ...request.headers,
+          ["x-access-token"]: this.authenticator.accessToken,
+        };
+
+        if (request.baseURL) {
+          // This will contruct a valid url in case the url starts with a '/' and the baseUrl end withs a '/'
+          request.url = new URL(request.url || "", request.baseURL).href;
+        }
+
+        return request;
       },
-    });
+      (err) => Promise.reject(err)
+    );
   }
 
-  public async put(url: string, data: any, headers: any): Promise<any> {
+  public async get<T = any>(url: string, headers?: Record<string, any>): Promise<AxiosResponse<T>> {
     await this.authenticator.validateAuthentication();
-    return axios.put(url, data, {
-      headers: {
-        ...(headers || {}),
-        "x-access-token": this.authenticator.accessToken,
-      },
-    });
+    return this.axios.get(url, { headers });
   }
 
-  public async delete(url: string, headers: any): Promise<any> {
+  public async post<T = any>(url: string, data: any, headers?: Record<string, any>): Promise<AxiosResponse<T>> {
     await this.authenticator.validateAuthentication();
-    return axios.delete(url, {
-      headers: {
-        ...(headers || {}),
-        "x-access-token": this.authenticator.accessToken,
-      },
-    });
+    return this.axios.post(url, data, { headers });
   }
 
-  public async patch(url: string, headers: any): Promise<any> {
+  public async put<T = any>(url: string, data: any, headers?: Record<string, any>): Promise<AxiosResponse<T>> {
     await this.authenticator.validateAuthentication();
-    return axios.patch(url, {
-      headers: {
-        ...(headers || {}),
-        "x-access-token": this.authenticator.accessToken,
-      },
-    });
+    return this.axios.put(url, data, { headers });
+  }
+
+  public async delete<T = any>(url: string, headers?: Record<string, any>): Promise<AxiosResponse<T>> {
+    await this.authenticator.validateAuthentication();
+    return this.axios.delete(url, { headers });
+  }
+
+  public async patch<T = any>(url: string, data: any, headers?: Record<string, any>): Promise<AxiosResponse<T>> {
+    await this.authenticator.validateAuthentication();
+    return this.axios.patch(url, data, { headers });
   }
 }
