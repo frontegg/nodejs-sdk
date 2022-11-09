1327bcc729ffc26461a4e40085b657e2e4840124
Eran Kricheli <eran.kricheli@frontegg.com>
2022-11-09 19:19:47 +0200

    fix: re-export IdentityClient

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { FronteggAuthenticator } from './src/authenticator';
 import { FronteggContext } from './src/components/frontegg-context';
 import { withAuthentication } from './src/middlewares';
-import { AuditsClient, HttpClient } from './src/clients';
+import { AuditsClient, HttpClient, IdentityClient } from './src/clients';
 
-export { AuditsClient, FronteggContext, FronteggAuthenticator, withAuthentication, HttpClient };
+export { AuditsClient, FronteggContext, FronteggAuthenticator, withAuthentication, HttpClient, IdentityClient };
