3ec0c67c01255379552248be8a886b5de9d960f5
Artur Wolny <artur.wolny@frontegg.com>
2023-08-01 14:28:46 +0300

    ci(sdk): fixed tests in CI

--- ci/run-test-suite.sh
+++ ci/run-test-suite.sh
@@ -4,7 +4,7 @@ set -e
 
 docker compose -p nodejs-sdk-tests up -d --wait
 
-npm run --prefix ../ test:jest
+npm run --prefix ../ test:jest --coverage
 RESULT=$@
 
 docker compose -p nodejs-sdk-tests down
